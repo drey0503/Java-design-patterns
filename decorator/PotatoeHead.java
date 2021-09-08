764aefa3bd595d3fd04f430c5d3fb861f060bf0d
Dreyson <dreyson@email.sc.edu>
2021-09-07 22:33:42 -0400

    Finished the potato head class and strings... can't compile yet.

--- decorator/PotatoeHead.java
+++ decorator/PotatoeHead.java
@@ -1,8 +1,33 @@
 package decorator;
 /**
  * @author Dreyson Clark
- * Responsible constructor for adding all of the lines to sections.
+ *  constructor responsible for adding all of the lines to sections.
  */
-public class PotatoeHead {
-    
+public class PotatoeHead extends Character {
+   public PotatoeHead() {
+    for(int i=0; i<7; i++){
+         if(i==0){
+             sections.add(" ");
+         }  
+         if(i==1){
+            sections.add("    ____      ");
+        }  
+        if(i==2){
+            sections.add("  /      \\  ");
+        }  
+        if(i==3){
+            sections.add(" |        |  ");
+        }  
+        if(i==4){
+            sections.add(" |        |  ");
+        }  
+        if(i==5){
+            sections.add("  \\      /  ");
+        }  
+        if(i==6){
+            sections.add("   \\____/  ");
+        }  
+    }
+}
+
 }
