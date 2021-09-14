29dcce561f7ebeb7c99b119099f43fccd9c20ff3
Dreyson <dreyson@email.sc.edu>
2021-09-14 13:14:31 -0400

    added constructor and some other things... Class is starting brb.

--- factory-design/HousePlan.java
+++ factory-design/HousePlan.java
@@ -7,4 +7,14 @@ import java.util.ArrayList;
 public abstract class HousePlan {
     protected ArrayList<String> material;
     protected ArrayList<String> features;
+    private int numRooms;
+    private int numWindows;
+    private int squareFeet;
+
+    public HousePlan(int numRooms, int numWindows, int squareFeet){
+        
+    }
+    public abstract void setMaterials();
+    public abstract void setFeatures();
+    
 }
