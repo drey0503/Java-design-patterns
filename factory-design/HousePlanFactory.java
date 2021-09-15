1e0918764960b1bbc96746ad43e31206bba35d7a
Dreyson <dreyson@email.sc.edu>
2021-09-15 09:46:39 -0400

    finished most of the work for the house plan factory, currently saving progress.

--- factory-design/HousePlanFactory.java
+++ factory-design/HousePlanFactory.java
@@ -1,12 +1,29 @@
-
+import java.util.ArrayList;
 
 /**
- * @author Dreyson Clark
- * + createHousePlan(String typel): HousePlan
+ * @author Dreyson Clark 
+ * This class creates the base for a house plan. 
  */
-public class HousePlanFactory{
+public class HousePlanFactory {
+    /**
+     * This method reads the string and if the string reads a specified 
+     * type of house plan, then the base houseplan is changed into the houseplan for that 
+     * type of house.
+     * @param type
+     * @return
+     */
+    public HousePlan createHousePlan(String type) {
+        HousePlan houseplan;
+        if (type.equalsIgnoreCase("logCabin")) {
+            houseplan = new LogCabinPlan();
+        }
+        else if (type.equalsIgnoreCase("tinyHouse")){
+            houseplan= new TinyHousePlan();
+        }
+         else if (type.equalsIgnoreCase("contemporaryHouse")){
+            houseplan= new TinyHousePlan();
+        }
+        return houseplan;
+    }
 
-public static void createHousePlan(String typel){
-  
-}
 }
