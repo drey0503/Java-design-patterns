d512b0c0b5589931c0a3af4ea3fc8d44fa234ef2
Dreyson <dreyson@email.sc.edu>
2021-11-18 13:55:17 -0500

    added state design pattern

---
+++ State Design Pattern/ArithemeticGame.java
@@ -0,0 +1,10 @@
+
+public class ArithemeticGame {
+private State easyState;
+
+    public ArithemeticGame(State state){
+    easyState = new State();
+    mediumState= new State();
+    hardState = new State();
+    }
+}
